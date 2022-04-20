0e8e5da58f2125fd91838eaa456b109ec43ce1da
lvzhenchao <1370164516@qq.com>
2022-04-20 22:49:39 +0800

    Create success.php

---
+++ ticket/success.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width; initial-scale=1.0" />
+    <title>在线选座订座（影院版）</title>
+    <meta name="keywords" content="jQuery,选座" />
+    <link href="./css/style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <a href="index.php?status=success">选座成功</a>
+</body>
+
+</html>
