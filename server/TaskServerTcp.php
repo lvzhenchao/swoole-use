a0d36d05b9c7f8785b87f6af91289adbcf1356c0
lvzhenchao <1370164516@qq.com>
2022-04-20 13:43:32 +0800

    Update TaskServerTcp.php

--- server/TaskServerTcp.php
+++ server/TaskServerTcp.php
@@ -27,19 +27,6 @@ class TaskServerTcp
             echo "Swoole http server is started".PHP_EOL;
         });
 
-//        //监听请求
-//        $this->server->on("request", function ($request, $response) {
-//
-//            //投递异步任务
-//            //需要执行很耗时的操作时，可以投递一个异步任务到TaskWorker进程池中执行，不影响当前请求的处理速度
-//            //模拟耗时10S的场景
-//            $data = [
-//                'task' => 'sendSms'
-//            ];
-//            $task_id = $this->server->task($data);
-//            $response->end("hello end");
-//        });
-
         $this->server->on("receive", function ($serv, $fd, $reactor_id, $data){//这个方法是TCP协议运行时必须有的
 
             $data = [
