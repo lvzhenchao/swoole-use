578f6647cf6fd15f2f60a987d57e3e8cb6638479
lvzhenchao <1370164516@qq.com>
2022-04-24 22:54:06 +0800

    Update index.php

--- swoole-barrage/index.php
+++ swoole-barrage/index.php
@@ -15,42 +15,38 @@
 <script>
 const dp = new DPlayer({
 	container: document.getElementById('dplayer'),
-    video: {
-	    url: '怀念青春.mp4',
-        pic: '111.jpg'
+	live: true,
+    danmaku: true,
+    apiBackend: {
+        read: function() {
+			ws = new WebSocket('ws://192.168.33.10:9505');
+			ws.onopen=function(){
+				console.log('success connect');
+			}
+        },
+        send: function(endpoint) {
+			console.log(endpoint.data.text);
+
+			ws.send(endpoint.data.text);
+        },
     },
-	// live: true,
-    // danmaku: true,
-    // apiBackend: {
-    //     read: function() {
-	// 		ws = new WebSocket('ws://192.168.67.142:8888');
-	// 		ws.onopen=function(){
-	// 			console.log('success connect');
-	// 		}
-    //     },
-    //     send: function(endpoint) {
-	// 		console.log(endpoint.data.text);
-    //
-	// 		ws.send(endpoint.data.text);
-    //     },
-    // },
-    //
-    // video: {
-    //     url: 'http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8',
-	// 	pic:'111.jpg',
-    //     type: 'hls',
-    // },
+
+    video: {
+        url: '怀念青春.mp4',//http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8',
+		pic:'111.jpg',
+        type: 'hls',
+    },
 });
 
-// ws.onmessage=function(e){
-//
-// 	const danmaku = {
-//     	text: e.data,
-//     	color: '#fff',
-//     	type: 'right',
-// 	};
-// 	dp.danmaku.draw(danmaku);
-// }
+ws.onmessage=function(e){
+
+	const danmaku = {
+    	text: e.data,
+    	color: '#fff',
+    	type: 'right',
+	};
+	dp.danmaku.draw(danmaku);
+}
 
 
 </script>
